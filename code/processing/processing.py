3d25d86f828d84ac46b270366ae8e2e9423c72c3
Adrien Pavao <adrien.pavao@u-psud.fr>
2018-08-21 19:51:45 +0200

    Correction on categorical variable detection when they are represented by numerical values

--- code/processing/processing.py
+++ code/processing/processing.py
@@ -14,15 +14,19 @@ def get_types(df):
     x = df.copy()
     dtypes = list()
     
+    n_rows = df.shape[0]
+    prop = int(n_rows / 20) # Arbitrary proportion of different values where a numerical variable is considered categorical
+    
     for column in x.columns:
         n = len(x[column].unique())
-        try:
-            sum = x[column].sum()
-        except:
-            sum = -np.inf
+        #try:
+        #    s = x[column].sum()
+        #except:
+        #    s = -np.inf
         if n == 2:
             dtypes.append('Binary')
-        elif (n > 2 and (sum == n*(n-1)/2 or sum == n*(n+1)/2)) or any(isinstance(i, str) for i in x[column]):
+        #elif (n > 2 and (sum == n*(n-1)/2 or sum == n*(n+1)/2)) or any(isinstance(i, str) for i in x[column]): ???
+        elif (n > 2 and (n <= prop)) or any(isinstance(i, str) for i in x[column]):
             dtypes.append('Categorical')
         else:
             dtypes.append('Numerical')
